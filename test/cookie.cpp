6571247204f5e8c7f789fc38c27ad6c9d7e17f3e
Giuseppe <JosephP91@users.noreply.github.com>
2015-11-04 15:07:58 +0100

    Create cookie.cpp
    Example on how to use cookies

---
+++ test/cookie.cpp
@@ -0,0 +1,35 @@
+#include "curl_easy.h"
+#include "curl_form.h"
+#include "curl_cookie.h"
+
+using curl::curl_header;
+using curl::curl_easy;
+using curl::curl_cookie;
+
+int main() {
+    // Easy object to handle the connection.
+    curl_easy easy;
+    // Set the environment
+    easy.add<CURLOPT_URL>("http://example.com");
+    easy.add<CURLOPT_VERBOSE>(1L);
+    // Create a cookie object.
+    curl_cookie cookie(easy);
+    // Let's set some cookies.
+    cookie.set_cookie_list("Set-Cookie: OLD_PREF=3d141414bf4209321; "
+                           "expires=Sun, 17-Jan-2038 19:14:07 GMT; path=/; domain=.example.com");
+    curlcpp_cookies cookies;
+    try {
+        easy.perform();
+        // Retrieve all the cookies for the example.com
+        cookies = cookie.get();
+        // Delete all the memory helded cookies.
+        easy.add<CURLOPT_COOKIEFILE>("ALL");
+    } catch (curl_easy_exception error) {
+        error.print_traceback();
+    }
+    // Now let's print them
+    for (auto cook : cookies) {
+        cout<<cook<<endl;
+    }
+    return 0;
+}
