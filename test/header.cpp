11c8f03d5010e97a69902218ef11c4770405a56a
Giuseppe <JosephP91@users.noreply.github.com>
2015-11-04 11:28:24 +0100

    Create header.cpp
    Example on how to use headers.

---
+++ test/header.cpp
@@ -0,0 +1,34 @@
+#include "curl_easy.h"
+#include "curl_form.h"
+#include "curl_header.h"
+
+using curl::curl_header;
+using curl::curl_easy;
+
+int main() {
+    // Let's create an object which will contain a list of headers.
+    curl_header header;
+    // Easy object to handle the connection.
+    curl_easy easy;
+
+    // Add custom headers.
+    header.add("Accept:");
+    header.add("Another:yes");
+    header.add("Host: example.com");
+    
+    // Add the headers to the easy object.
+    easy.add<CURLOPT_HTTPHEADER>(header.get());
+    
+    easy.add<CURLOPT_URL>("localhost");
+    easy.add<CURLOPT_VERBOSE>(1L);
+    try {
+        // Request execution
+        easy.perform();
+    } catch (curl_easy_exception error) {
+        // If you want to get the entire error stack we can do:
+        curlcpp_traceback errors = error.get_traceback();
+        // Otherwise we could print the stack like this:
+        error.print_traceback();
+    }
+    return 0;
+}
